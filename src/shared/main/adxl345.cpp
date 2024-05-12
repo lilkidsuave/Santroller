6674efeffb5477de67d03c6848ce8658ee43c65c
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-05-12 01:11:39 -0400

    Update adxl345.cpp

--- src/shared/main/adxl345.cpp
+++ src/shared/main/adxl345.cpp
@@ -9,9 +9,14 @@ void init_adxl() {
     twi_writeSingleToPointer(ADXL_TWI_PORT, ADXL345_ADDRESS, ADXL345_DATA_FORMAT, 0x0B);
 }
 void tick_adxl() {
+    int16_t filtered[3] = {0};  // Initialize filtered data array
     int16_t raw[3];
     twi_readFromPointer(ADXL_TWI_PORT, ADXL345_ADDRESS, ADXL345_DATAX0, 6, (uint8_t*)raw);
     for (int i = 0; i < 3; i++) {
+        if (i == 1) {  // Check if Y-axis
+            raw[i] *= -1;  // Invert Y-axis value
+        }
+        // Apply filtering and store in filtered array
         filtered[i] = (raw[i] * 64) * LOW_PASS_ALPHA + (filtered[i] * (1.0 - LOW_PASS_ALPHA));
     }
 }
